4c34072d4d5e30527663c6b23727c6d559061124
chask96969 <160220a009@gmail.com>
2023-07-04 15:50:05 +0530

    success

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Communal Web
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
